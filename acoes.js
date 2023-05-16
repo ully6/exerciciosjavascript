e2c9f0600fa017b2dba50f27df1f2a03bd029f02
Ulysses <128516897+ully6@users.noreply.github.com>
2023-05-16 15:44:01 -0300

    Add files via upload

---
+++ acoes.js
@@ -0,0 +1,16 @@
+function acao1(){
+    let resp = window.document.getElementById('saida')
+    resp.innerHTML += `<p>Clicou no primeiro botão</p>`
+}
+function acao2(){
+    let resp = window.document.getElementById('saida')
+    resp.innerHTML += '<p>Clicou no segundo botão</p>'
+}
+function acao3(){
+    let resp = window.document.getElementById('saida')
+    resp.innerHTML += '<p>Clicou no terceiro botão</p>'
+}
+function acao4(){
+    let resp = window.document.getElementById('saida')
+    resp.innerHTML += '<p>Clicou no quarto botão</p>'
+}
